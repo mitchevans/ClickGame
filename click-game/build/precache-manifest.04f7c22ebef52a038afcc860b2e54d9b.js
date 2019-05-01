b4cb26a12762f2e1a7bc120e92a2584a28bde8e5
Mitch <mrmtevans@gmail.com>
2019-04-30 19:27:04 -0700

    added build

---
+++ click-game/build/precache-manifest.04f7c22ebef52a038afcc860b2e54d9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fd1822d60b048c8a6b3a7be3e06b18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8873183fcc576c608314",
+    "url": "/static/css/main.acc74b6b.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "8873183fcc576c608314",
+    "url": "/static/js/main.c71057d3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
